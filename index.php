db49551d89be30676de81e3e37b7905940b027f3
MBernackiProgrammer <101584672+MBernackiProgrammer@users.noreply.github.com>
2022-10-03 19:54:11 +0200

    Wstęp

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Noxy Cofee</title>
+
+    <link rel="stylesheet" href="UpBar.css">
+    <link rel="stylesheet" href="MidleBar.css">
+    <link rel="stylesheet" href="DownBar.css">
+</head>
+<body>
+    <div class="UpBar">
+        <div class="Logo"></div>
+        <div></div> 
+    </div>
+
+    <div class="MiddleBar">
+        <div class="CellMidleBar"></div>
+        <div class="CellMidleBar"></div>
+        <div class="CellMidleBar"></div>
+    </div>
+
+    <div class="DownBar">
+        <div class="Half">
+            <div class="UpName">Nasi dostawcy</div>
+            <div class="Company">
+                <div class="CompanyFoto">
+
+                </div>
+                <div class="CompanyRight">
+                    <div class="CompanyName">
+
+                    </div>
+                    <div class="CompanyDiscrabe">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a quam massa. Donec cursus eu dolor id fermentum. Aliquam maximus elit vitae libero tincidunt lacinia. Nulla nec risus vulputate, rutrum sapien eget, placerat nisl. Vestibulum venenatis justo vehicula dapibus bibendum. Sed commodo libero sit amet aliquet aliquam. Phasellus vitae erat nisi. Nulla facilisi. Cras sem magna, tempor ac urna et, rutrum bibendum nulla. Donec vel convallis nisi. Nullam facilisis, libero in placerat scelerisque, augue nisi congue leo, eu congue mi lacus quis nisl. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce sed purus faucibus, posuere arcu quis, tincidunt ante. Cras pharetra, lacus ut euismod porttitor, arcu mi fringilla nulla, at rhoncus ligula risus et justo. Pellentesque eget pharetra justo.
+                    </div>
+                </div>
+                
+            </div>
+            
+
+        </div>
+        <div class="Half">Opinie naszych klientów</div>
+    </div>
+</body>
+</html>
